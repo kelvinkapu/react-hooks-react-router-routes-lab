58a865a5a57723567bc2f161ca3ceab1d04e0965
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-08-06 17:49:06 +0300

    Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
+import { Route,} from "react-router-dom";
+// import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <Route path="/">
+  <Home />
+  <Movies/>
+  <Directors/>
+  <Actors/>
+  <Home/>
+</Route>
+  
+  </div>;
 }
 
 export default App;
